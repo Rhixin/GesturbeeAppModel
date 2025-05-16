6448872b277ada0c37ba6d951ba1459a6e2d8fc6
Rhixin <vallestedted@gmail.com>
2025-05-16 09:49:26 +0800

    Added only camera

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,64 @@
 "use client";
 import HandTracker from "@/components/HandTracker";
-import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { io } from "socket.io-client";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [handData, setHandData] = useState<number[] | null>(null);
   const [prediction, setPrediction] = useState<any>(null);
+  const [socketStatus, setSocketStatus] = useState<
+    "connecting" | "connected" | "disconnected" | "error"
+  >("disconnected");
+  const socketRef = useRef<any>(null);
+
+  useEffect(() => {
+    const socket = io("https://aslmodelbackend.onrender.com/");
+
+    socketRef.current = socket;
+
+    setSocketStatus("connecting");
+
+    // Socket event handlers
+    socket.on("connect", () => {
+      console.log("Socket connected");
+      setSocketStatus("connected");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected");
+      setSocketStatus("disconnected");
+    });
+
+    socket.on("connect_error", (error) => {
+      console.error("Connection error:", error);
+      setSocketStatus("error");
+    });
+
+    socket.on("prediction_result", (result) => {
+      setPrediction(result);
+    });
+
+    socket.on("prediction_error", (error) => {
+      console.error("Prediction error:", error);
+    });
+
+    // Clean up on component unmount
+    return () => {
+      socket.disconnect();
+      socketRef.current = null;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (handData && socketRef.current && socketRef.current.connected) {
+      socketRef.current.emit("hand_data", handData);
+    }
+
+    console.log(prediction);
+  }, [handData]);
 
   return (
-    <div className="p-10">
+    <div className="flex justify-center items-center h-[100vh] w-full">
       <HandTracker handData={handData} setHandData={setHandData} />
     </div>
   );
